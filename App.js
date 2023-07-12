813bbdd72fef76348d4797f89ae24b48c986d1d0
sounddrill31 <sounddrill31@gmail.com>
2023-07-12 13:59:48 +0530

    Update App.js

--- App.js
+++ App.js
@@ -20,16 +20,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-//import * as React from 'react';
-//import { WebView } from'react-native-webview';
-//
-//export default class App extends React.Component {
-//  render() {
-//    return (
-//      <WebView
-//      source={{ uri: google.com }}
-//      style={{ marginTop: 20 }}
-//      />
- //   );
- // }
-//}
